12029a9902a977dc79a362d75d15396b030027df
lalabhi <abhishek.lal@qed42.com>
2019-06-27 20:34:43 +0530

    adding the reset for the node

---
+++ qed42test/src/Plugin/rest/resource/ConfigResource.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace Drupal\extramodule\Plugin\rest\resource;
+
+use Drupal\Core\Session\AccountProxyInterface;
+use Drupal\rest\Plugin\ResourceBase;
+use Drupal\rest\ResourceResponse;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use Psr\Log\LoggerInterface;
+use Drupal\Component\Serialization\Json;
+
+/**
+ * Provides a Demo Resource
+ *
+ * @RestResource(
+ *   id = "demo_resource",
+ *   label = @Translation("Demo Resource"),
+ *   uri_paths = {
+ *     "canonical" = "/node/{nid}/{appid}",
+ *   }
+ * )
+ */
+class ConfigResource extends ResourceBase {
+
+  protected $currentUser;
+
+  /**
+   * Constructs a Drupal\rest\Plugin\ResourceBase object.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin_id for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param array $serializer_formats
+   *   The available serialization formats.
+   * @param \Psr\Log\LoggerInterface $logger
+   *   A logger instance.
+   * @param \Drupal\Core\Session\AccountProxyInterface $current_user
+   *   A current user instance.
+   */
+  public function __construct(
+    array $configuration,
+    $plugin_id,
+    $plugin_definition,
+    array $serializer_formats,
+    LoggerInterface $logger,
+    AccountProxyInterface $current_user) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition, $serializer_formats, $logger);
+    $this->currentUser = $current_user;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->getParameter('serializer.formats'),
+      $container->get('logger.factory')->get('rest'),
+      $container->get('current_user')
+    );
+  }
+
+  public function get($nid, $apid) {
+    $site_id = \Drupal::config('system.site')->get('apid');
+
+
+    if($site_id == $apid){
+      $node = $this->entityTypeManager->getStorage('node')->load($nid);
+      $myJSON = Json::encode($node);
+      $response = ['message' => $myJSON];
+      return new ResourceResponse($response);
+    }
+    $response = ['message' => 'no access to this part'];
+    return new ResourceResponse($response);
+
+
+  }
+
+
+
+
+}
